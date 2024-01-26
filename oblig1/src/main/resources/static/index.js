0e67a25e1c31008b988f314f8537515f4f77f6a6
TobiasFriis <tofri9269@oslomet.no>
2024-01-26 15:09:34 +0100

    andre utkast

--- oblig1/src/main/resources/static/index.js
+++ oblig1/src/main/resources/static/index.js
@@ -1,13 +1,52 @@
 let filmer = [];
 
+
+function innholdSjekk(film, antall, fornavn, etternavn, telefonnr, epost){
+    let sjekk = true;
+    if (antall == null || antall === ""){
+        manglerInnhold("antall");
+        sjekk = false;
+    }
+    if(fornavn == null || fornavn === ""){
+        manglerInnhold("fornavn");
+        sjekk = false;
+    }
+    if (etternavn == null || etternavn === ""){
+        manglerInnhold("etternavn");
+        sjekk = false;
+    }
+    if(telefonnr == null || telefonnr === ""){
+        manglerInnhold("telefonnr");
+        sjekk = false;
+    }
+    if(epost == null || epost === ""){
+        manglerInnhold("epost");
+        sjekk = false;
+    }
+    return sjekk;
+}
+function manglerInnhold(input){
+    document.getElementById(input+"Mangler").innerHTML = "Må skrive noe inn i "+input;
+}
+function fjernManglerInnhold(){
+    document.getElementById("antallMangler").innerHTML = "";
+    document.getElementById("fornavnMangler").innerHTML = "";
+    document.getElementById("etternavnMangler").innerHTML = "";
+    document.getElementById("telefonnrMangler").innerHTML = "";
+    document.getElementById("epostMangler").innerHTML = "";
+}
 function kjopBilett(){
-    let bestilling = [];
+    fjernManglerInnhold();
     let film = document.getElementById("filmer").value;
     let antall = document.getElementById("antall").value;
     let fornavn = document.getElementById("fornavn").value;
     let etternavn = document.getElementById("etternavn").value;
     let telefonnr = document.getElementById("telefonnr").value;
     let epost = document.getElementById("epost").value;
+    if (innholdSjekk(film, antall, fornavn, etternavn, telefonnr, epost) === false){
+        return;
+    }
+    let bestilling = [];
     bestilling.push(film, antall, fornavn, etternavn, telefonnr, epost);
     filmer.push(bestilling);
     let liste = document.getElementById("filmListe");
@@ -19,6 +58,7 @@ function kjopBilett(){
     }
 }
 function slettBiletter(){
+    fjernManglerInnhold();
     filmer = [];
     document.getElementById("filmListe").innerText = "";
     document.getElementById("antall").value = "";
